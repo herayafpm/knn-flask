89b2e26111da2e6008e079add2cd820c3d1df92e
Heraya <herayafpm@gmail.com>
2021-03-05 19:46:28 +0700

    update file

--- app/__init__.py
+++ app/__init__.py
@@ -1,158 +1,87 @@
 from flask import Flask, request, abort, jsonify
 import numpy as np
 import glob
-from PIL import Image
+from PIL import Image, ImageFile
+from skimage.feature import greycomatrix, greycoprops
 import pickle
 from os.path import isfile
 from io import BytesIO
 import base64
+import os
+from flask_cors import CORS, cross_origin
 
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-def test(datatest, clf):
-    feature_datatest = []
-    y = []
-    for data in datatest:
-        if 'kambing' in data:
-            y.append(0)
-        else:
-            y.append(1)
-        feature_datatest.append(extract_feature(data))
-    X = np.vstack(feature_datatest)
-    return clf.score(X, y)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def extract_feature(path):
+    img = Image.open(path)
+    img = img.resize([128,128])
+    img = img.convert('L')
+    img = np.array(img)
+    feature = []
+    glcm = greycomatrix(img,[4,5],[0,np.pi/4,np.pi/2,3*np.pi/4],symmetric=True,normed=False)
+    contrast = greycoprops(glcm,'contrast')
+    homogeneity = greycoprops(glcm,'homogeneity')
+    energy = greycoprops(glcm,'energy')
+    asm = greycoprops(glcm,'ASM')
+    feature.append(contrast[0][0])
+    feature.append(contrast[0][1])
+    feature.append(contrast[0][2])
+    feature.append(contrast[0][3])
 
-def extract_feature(img):
-    img = Image.open(img).convert('L')
-    gl_0 = glcm(img, 0)
-    gl_45 = glcm(img, 45)
-    gl_90 = glcm(img, 90)
-    gl_135 = glcm(img, 135)
-    feature = np.array([
-        np.average([contrast(gl_0), energy(gl_0),
-                    homogenity(gl_0), entrophy(gl_0)]),
-        np.average([contrast(gl_45), energy(gl_45),
-                    homogenity(gl_45), entrophy(gl_45)]),
-        np.average([contrast(gl_90), energy(gl_90),
-                    homogenity(gl_90), entrophy(gl_90)]),
-        np.average([contrast(gl_135), energy(gl_135),
-                    homogenity(gl_135), entrophy(gl_135)])
-    ])
+    feature.append(homogeneity[0][0])
+    feature.append(homogeneity[0][1])
+    feature.append(homogeneity[0][2])
+    feature.append(homogeneity[0][3])
+
+    feature.append(energy[0][0])
+    feature.append(energy[0][1])
+    feature.append(energy[0][2])
+    feature.append(energy[0][3])
+
+    feature.append(asm[0][0])
+    feature.append(asm[0][1])
+    feature.append(asm[0][2])
+    feature.append(asm[0][3])
     return feature
-
-
-def extract_feature(img):
-    img = Image.open(BytesIO(base64.b64decode(img))).convert('L')
-    gl_0 = glcm(img, 0)
-    gl_45 = glcm(img, 45)
-    gl_90 = glcm(img, 90)
-    gl_135 = glcm(img, 135)
-    feature = np.array([
-        np.average([contrast(gl_0), energy(gl_0),
-                    homogenity(gl_0), entrophy(gl_0)]),
-        np.average([contrast(gl_45), energy(gl_45),
-                    homogenity(gl_45), entrophy(gl_45)]),
-        np.average([contrast(gl_90), energy(gl_90),
-                    homogenity(gl_90), entrophy(gl_90)]),
-        np.average([contrast(gl_135), energy(gl_135),
-                    homogenity(gl_135), entrophy(gl_135)])
-    ])
-    return feature
-
-
-def contrast(matrix):
-    width, height = matrix.shape
-    res = 0
-    for i in range(width):
-        for j in range(height):
-            res += matrix[i][j] * np.power(i-j, 2)
-    return res
-
-
-def energy(matrix):
-    width, height = matrix.shape
-    res = 0
-    for i in range(width):
-        for j in range(height):
-            res += np.power(matrix[i][j], 2)
-    return res
-
-
-def homogenity(matrix):
-    width, height = matrix.shape
-    res = 0
-    for i in range(width):
-        for j in range(height):
-            res += matrix[i][j] / (1+np.power(i-j, 2))
-    return res
-
-
-def entrophy(matrix):
-    width, height = matrix.shape
-    res = 0
-    for i in range(width):
-        for j in range(height):
-            if matrix[i][j] > 0:
-                res += matrix[i][j] * np.log2(matrix[i][j])
-    return res
-
-
-def glcm(img, degree):
-    img = img.resize([128, 128], Image.NEAREST)
-    arr = np.array(img)
-    res = np.zeros((arr.max() + 1, arr.max() + 1), dtype=int)
-    width, height = arr.shape
-    if degree == 0:
-        for i in range(width - 1):
-            for j in range(height):
-                res[arr[j, i+1], arr[j, i]] += 1
-    elif degree == 45:
-        for i in range(width - 1):
-            for j in range(1, height):
-                res[arr[j-1, i+1], arr[j, i]] += 1
-    elif degree == 90:
-        for i in range(width):
-            for j in range(1, height):
-                res[arr[j-1, i], arr[j, i]] += 1
-    elif degree == 135:
-        for i in range(1, width):
-            for j in range(1, height):
-                res[arr[j-1, i-1], arr[j, i]] += 1
-    else:
-        print("sudut tidak valid")
-    return res
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
 @app.route('/index')
+@cross_origin()
 def index():
     return "Hello World"
 
 
 @app.route('/proses', methods=['GET', 'POST'])
+@cross_origin()
 def proses():
-    if(request.method == "POST"):
-        data = request.json
-        # datasets = []
-        # datatests = []
-        # for tipe in ['kambing', 'oplosan']:
-        #     for i in range(0, 33):
-        #         datasets.append("./data/{0}.{1}.jpg".format(tipe, i))
-        #     for i in range(33, 50):
-        #         datatests.append("./data/{0}.{1}.jpg".format(tipe, i))
-        # datasets = [f for f in datasets if isfile(f)]
-        # datatests = [f for f in datatests if isfile(f)]
-        loaded_model = pickle.load(open('./finalized_model.sav', 'rb'))
-        # result = test(datatests, loaded_model)
-        predict = loaded_model.predict([extract_feature(data['img'])])
-        if predict[0] == 0:
-            result = "Kambing"
-        else:
-            result = "Oplosan"
-
+    if request.method == "POST":
+        result = ""
+        if 'file' not in request.files:
+            result = "No File"
+        file = request.files['file']
+        if file.filename == '':
+            result = "No File"
+        if file and allowed_file(file.filename):
+            file_predict = extract_feature(file)
+            loaded_model = pickle.load(open('./finalized_model.sav', 'rb'))
+            predict = loaded_model.predict(np.array([file_predict]))
+            if predict[0] == 0:
+                result = "Kambing"
+            else:
+                result = "Oplosan"
         return jsonify({"result": result}), 200
+        
     else:
         return "Hello World"
 
